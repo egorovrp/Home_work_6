2a464853700ae6ec8cf32c4bd107d5807dd63cff
Roman_Egorov <egorovrp1991@mail.ru>
2023-07-27 23:33:29 +0300

    Home Work 6

---
+++ Quest1/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int result = int. Parse(Console.ReadLine()!);
+    return result;
+}
+
+int[] InputArray(int size)
+{
+    int[] array = new int[size];
+    for(int i=0; i<array.Length; i++)
+    {
+        array[i] = Prompt($"Введите {i+1}-й элемент: ");
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i< array.Length; i++)
+    {
+        Console.WriteLine(($"Число[{i}] = {array[i]}"));
+    }
+}
+
+int SearchNumber(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+int size = Prompt("Введите количество элементов массива: ");
+int [] array = InputArray(size);
+PrintArray(array);
+Console.WriteLine($"Кол-во чисел больше 0 = {SearchNumber(array)}");
---
+++ Quest2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+
+double Prompt(string message)
+{
+    Console.Write(message);
+    double result = double. Parse(Console.ReadLine()!);
+    return result;
+}
+
+double SearchX(double a, double b, double c, double d)
+{
+    double x = (a-b)/(c-d);
+    return x;
+}
+
+double SearchY(double a, double b, double c)
+{
+     double y = a*b+c;
+    return y;
+}
+
+double b1 = Prompt("Введите коэффициент b1 первой прямой: ");
+double k1 = Prompt("Введите коэффициент k1 первой прямой: ");
+double b2 = Prompt("Введите коэффициент b2 второй прямой: ");
+double k2 = Prompt("Введите коэффициент k2 второй прямой: ");
+
+double X = SearchX(b2,b1,k1,k2);
+double Y = SearchY(k1,X,b1);
+
+Console.WriteLine($"Точка пересечения двух прямых: ({X},{Y})");
+
